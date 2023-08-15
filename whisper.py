75a7f23b12098a6b6666b7a9d61d885662d975e8
Pedram Tavadze <petavazohi@mix.wvu.edu>
2023-08-15 07:42:29 -0400

    small update

--- whisper.py
+++
@@ -1,9 +0,0 @@
-import os
-import openai
-
-key = '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'
-openai.api_key = key
-audio_file = open("audio.mp3", "rb")
-transcript = openai.Audio.transcribe("whisper-1", audio_file)
-
-
